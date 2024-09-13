347d6e7ffb05b0727c130111f8dcd76de4f9a54d
Daniel Papilion <papiliond.94@gmail.com>
2024-09-13 15:06:03 +0200

    Fix named export issue

--- plugins/contentful.ts
+++ plugins/contentful.ts
@@ -1,4 +1,4 @@
-import { createClient } from 'contentful';
+import contentful from 'contentful';
 
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
@@ -7,7 +7,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   return {
     provide: {
       contentful: {
-        createClient: () => createClient({
+        createClient: () => contentful.createClient({
           environment: nuxtApp.$config.public.contentful.environment,
           space: nuxtApp.$config.public.contentful.space,
           accessToken: nuxtApp.$config.public.contentful.accessToken,
