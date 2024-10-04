71ce518246ec4cfce0b55f185d495e10c24eca65
Daniel Papilion <papiliond.94@gmail.com>
2024-10-04 22:26:54 +0200

    Make tailwindcss work; Still jit mode not working.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
-const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
+import type { Config } from "tailwindcss";
+import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
+import typography from '@tailwindcss/typography';
 
 export default <Partial<Config>>{
 	theme: {
@@ -68,15 +69,26 @@ export default <Partial<Config>>{
 			},
 		},
 	},
+	mode: "jit",
+	purge: [
+		"./components/**/*.{js,vue,ts}",
+		"./composables/**/*.{js,vue,ts}",
+		"./domains/**/*.{js,vue,ts}",
+		"./layouts/**/*.vue",
+		"./pages/**/*.vue",
+		"./plugins/**/*.{js,ts}",
+		"./app.vue",
+		"./error.vue",
+	],
+	safelist: [
+		"max-md:hidden",
+		"md:max-lg:hidden",
+		"lg:hidden"
+	],
 	plugins: [
+		typography,
 		iconsPlugin({
-			// Select the icon collections you want to use
-			// You can also ignore this option to automatically discover all individual icon packages you have installed
-			// If you install @iconify/json, you should explicitly specify the collections you want to use, like this:
 			collections: getIconCollections(["carbon"]),
-			// If you want to use all icons from @iconify/json, you can do this:
-			// collections: getIconCollections("all"),
-			// and the more recommended way is to use `dynamicIconsPlugin`, see below.
 		}),
 	]
 };
