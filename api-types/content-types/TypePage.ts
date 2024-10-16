602b83484292b034c8e72b28a3d99e8744452d68
Vladimir <vladimir.miljkovic@itgcommerce.com>
2024-10-16 16:00:20 +0200

    Add TypeQuoteSkeleton to TypePageFields blocks

--- api-types/content-types/TypePage.ts
+++ api-types/content-types/TypePage.ts
@@ -1,12 +1,13 @@
 import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
 import type { TypeHeroSkeleton } from "./TypeHero";
 import type { TypeRowProductSkeleton } from "./TypeRowProduct";
+import type { TypeQuoteSkeleton } from "./TypeQuote";
 
 export interface TypePageFields {
     slug: EntryFieldTypes.Symbol;
     title: EntryFieldTypes.Symbol;
     description: EntryFieldTypes.Text;
-    blocks?: EntryFieldTypes.Array<EntryFieldTypes.EntryLink<TypeHeroSkeleton | TypeRowProductSkeleton>>;
+    blocks?: EntryFieldTypes.Array<EntryFieldTypes.EntryLink<TypeHeroSkeleton | TypeRowProductSkeleton | TypeQuoteSkeleton>>;
 }
 
 export type TypePageSkeleton = EntrySkeletonType<TypePageFields, "page">;
